42809af0b21014e8662bbe2f7ae3252b5e1e1dd2
Ahmed Hassop <Ahmed Hassob>
2024-11-03 17:09:05 +0200

    add contact-us feature and user can send messages

--- api/routes/contactRoute.js
+++ api/routes/contactRoute.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const isValidId = require("../../middlewares/isValidId");
-const authenticateUser = require("../../middlewares/authenticateUser");
+const isValidId = require("../middlewares/isValidId");
+const authenticateUser = require("../middlewares/authenticateUser");
 const {
   addContactMessage,
   getContactMessages,
   deleteMessage,
-} = require("../../controllers/contact");
+} = require("../controllers/contact");
 
 router.use(authenticateUser);
 router.post("/", addContactMessage);
